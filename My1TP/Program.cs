fd0116109c34019aeb41c5338c7da4a8543eb122
Ogannes <Vovan91146@mail.ru>
2022-12-03 14:52:48 +0300

    Добавил гитигнор

--- My1TP/Program.cs
+++ My1TP/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿bool isParsed = false;
+int number = 0;
+while (isParsed == false)
+{
+    isParsed = int.TryParse(Console.ReadLine(), out number);
+    if(isParsed == false)
+    {
+        Console.WriteLine("Пиши числа!");
+    }
+}
